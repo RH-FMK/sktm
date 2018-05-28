d6c149e996af1a57a1c53340c36f55e9aa9ce26c
Nikolai Kondrashov <Nikolai.Kondrashov@redhat.com>
2018-05-28 10:20:18 +0300

    Specify patch order for patch_url_list args

--- sktm/jenkins.py
+++ sktm/jenkins.py
@@ -113,7 +113,8 @@ class Project(object):
             buildid:    Jenkins build ID.
 
         Return:
-            The list of Patchwork patch URLs.
+            The list of Patchwork patch URLs, in the order the patches should
+            be applied in.
         """
         return self.__get_cfg_data(buildid, "skt.cmd_merge", "pw")
 
@@ -199,7 +200,8 @@ class Project(object):
                             or None if unknown.
             emails:         Set of e-mail addresses involved with the patchset
                             to send notifications to.
-            patch_url_list: List of URLs pointing to patches to apply.
+            patch_url_list: List of URLs pointing to patches to apply, in the
+                            order they should be applied in.
             makeopts:       String of extra arguments to pass to the build's
                             make invocation.
 
